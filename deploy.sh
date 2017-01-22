265370d75f939dd84024d5cb84b7429c2be1635b
Lucas Wonderley <lucas.wonderley@sap.com>
2017-01-22 10:18:03 -0500

    Keep synced and local scripts in Git repo
    This way copying isn't necessary.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
+set -e
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo Running $script_dir/${BASH_SOURCE}
+echo Adding bash_profile_synced to ~/.bash_profile.
+echo source $script_dir/bash_profile_synced >> ~/.bash_profile
+if [ ! -e $script_dir/bash_profile_local ]; then
+  echo Creating bash_profile_local.
+  cp -v $script_dir/bash_profile_local.template $script_dir/bash_profile_local
+fi
 if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
-cp -v $script_dir/.bash_profile $HOME
-if [ ! -e $HOME/.bash_profile_local ]; then
-  cp -v $script_dir/.bash_profile_local.template $HOME/.bash_profile_local
-fi
-cp -v $script_dir/.tmux.conf $HOME
-cp -v $script_dir/.vimrc $HOME
+echo Sourcing .bash_profile_synced.
+source $script_dir/bash_profile_synced
+
