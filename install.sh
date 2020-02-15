ed619ffe29acf2c2743c8c34961dfc168e7a233b
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2020-02-15 12:21:35 -0500

    Simplify this so it's useful to me
    Disable vim and tmux stuff
    
    Some other cleanup

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo Running $script_dir/${BASH_SOURCE}
 echo Adding bash_profile_synced to $HOME/.bash_profile.
 echo source $script_dir/bash_profile_synced >> $HOME/.bash_profile
-echo Adding tmux.conf_synced to $HOME/.tmux.conf.
-echo source $script_dir/tmux.conf_synced >> $HOME/.tmux.conf
-echo Adding vimrc_synced to $HOME/.vimrc.
-echo so $script_dir/vimrc_synced >> $HOME/.vimrc
+# Commenting all this stuff out because I only want the basic
+# .bash_profile configs and scripts, not fancy vim and tmux stuff
+# echo Adding tmux.conf_synced to $HOME/.tmux.conf.
+# echo source $script_dir/tmux.conf_synced >> $HOME/.tmux.conf
+# echo Adding vimrc_synced to $HOME/.vimrc.
+# echo so $script_dir/vimrc_synced >> $HOME/.vimrc
 if [ ! -e $script_dir/bash_profile_local ]; then
   echo Creating bash_profile_local.
   cp -v $script_dir/bash_profile_local.template $script_dir/bash_profile_local
@@ -24,11 +26,11 @@ fi
 #   echo Creating vimrc_local.
 #   touch $script_dir/vimrc_local
 # fi
-if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-  # Install plugins which are specified in vimrc_synced.
-  vim -c 'PluginInstall' -c 'qa!'
-fi
-echo Configurations installed. Now run this command:
+#if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
+#  git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+#  # Install plugins which are specified in vimrc_synced.
+#  vim -c 'PluginInstall' -c 'qa!'
+#fi
+echo "Configurations installed. Now run this command (or just start a new terminal session):"
 echo source $script_dir/bash_profile_synced
 
