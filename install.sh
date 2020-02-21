14b797eea8c7fbbd17c65faaf0e8cc6cfea8e2c8
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2020-02-21 15:45:57 -0500

    Remove bash_profile_local
    Can just put local stuff in .bash_profile or similar

--- install.sh
+++ install.sh
@@ -12,20 +12,6 @@ echo Adding tmux.conf_synced to $HOME/.tmux.conf.
 echo source $script_dir/tmux.conf_synced >> $HOME/.tmux.conf
 echo Adding vimrc_synced to $HOME/.vimrc.
 echo so $script_dir/vimrc_synced >> $HOME/.vimrc
-if [ ! -e $script_dir/bash_profile_local ]; then
-  echo Creating bash_profile_local.
-  cp -v $script_dir/bash_profile_local.template $script_dir/bash_profile_local
-fi
-# Currently no need for local (non-synced) versions
-# of these config files.
-# if [ ! -e $script_dir/tmux.conf_local ]; then
-#   echo Creating tmux.conf_local.
-#   touch tmux.conf_local
-# fi
-# if [ ! -e $script_dir/vimrc_local ]; then
-#   echo Creating vimrc_local.
-#   touch $script_dir/vimrc_local
-# fi
 if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
  git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
  # Install plugins which are specified in vimrc_synced.
